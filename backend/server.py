c1c14170c56874261d4ff317398ded3522bbf32c
leohutzl26-cyber <leohutzl26@gmail.com>
2026-02-20 15:46:01 -0300

    Update CORS origins for the server

--- backend/server.py
+++ backend/server.py
@@ -765,7 +765,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://movilizacion-hcu.onrender.com", "http://localhost:3000"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
